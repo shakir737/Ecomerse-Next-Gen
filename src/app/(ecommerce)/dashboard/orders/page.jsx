78354994018b99091b68d47d1eaa149fd30e25b3
shakir raza <shakirraza737@gmail.com>
2024-10-01 19:05:54 +0300

    initial changes for deployment

--- src/app/(ecommerce)/dashboard/orders/page.jsx
+++ src/app/(ecommerce)/dashboard/orders/page.jsx
@@ -2,7 +2,7 @@ import OrderList from "@/components/OrderList";
 import React from "react";
 import prisma from "@/utils/connection";
 const Orders = async () => {
-  const orders = await prisma.order.findMany({
+  const orders = await prisma?.order?.findMany({
     where: { isPaid: true },
     take: 10,
     skip: 0,
