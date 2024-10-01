436d9935c05bb8a146e4c4c4d9ec7d73b51c7bcf
shakir raza <shakirraza737@gmail.com>
2024-10-01 19:49:54 +0300

    initial changes

--- src/app/(ecommerce)/dashboard/orders/page.jsx
+++ src/app/(ecommerce)/dashboard/orders/page.jsx
@@ -2,14 +2,14 @@ import OrderList from "@/components/OrderList";
 import React from "react";
 import prisma from "@/utils/connection";
 const Orders = async () => {
-  const orders = await prisma?.order?.findMany({
-    where: { isPaid: true },
-    take: 10,
-    skip: 0,
-    include: { OrderItem: { include: { product: true } }, addressInfo: true },
-  });
+  // const orders = await prisma?.order?.findMany({
+  //   where: { isPaid: true },
+  //   take: 10,
+  //   skip: 0,
+  //   include: { OrderItem: { include: { product: true } }, addressInfo: true },
+  // });
 
-  return <OrderList orders={orders} />;
+  return <div></div>;
 };
 
 export default Orders;
