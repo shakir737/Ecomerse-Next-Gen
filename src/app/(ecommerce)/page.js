782bbe71ff579b07009422131c8c79d02eb5975f
shakir raza <shakirraza737@gmail.com>
2024-10-01 21:41:50 +0300

    initial changes

--- src/app/(ecommerce)/page.js
+++ src/app/(ecommerce)/page.js
@@ -8,39 +8,16 @@ export default async function Home() {
   const Carousels = dynamic(() => import("@/components/Carousels"));
   const Featured = dynamic(() => import("@/components/Featured"));
   const Enhancement = dynamic(() => import("@/components/Enhancement"));
-  // const query = {
-  //   take: 6,
-  //   skip: 0,
-  // };
-  // const categories = await prisma?.category?.findMany(query);
-  // const popularProducts = await prisma?.product?.findMany({
-  //   ...query,
-  //   orderBy: { views: "desc" },
-  // });
-  // const products = await prisma?.product?.findMany(query);
 
   return (
     <div className="">
       <Carousels />
       <div className="px-[10%]">
-        {/* {products && (
-          <Fashsales
-            title="Today's"
-            heading="Flash sales"
-            products={products}
-          />
-        )} */}
-
         <Separator className="my-4" />
-        {/* {categories && <CategoryList categories={categories} />} */}
 
         <Enhancement />
         <Separator className="my-4" />
-        {/* <Fashsales
-          title="Our Products"
-          heading="Explore Our Products"
-          products={popularProducts}
-        /> */}
+
         <Featured />
       </div>
     </div>
