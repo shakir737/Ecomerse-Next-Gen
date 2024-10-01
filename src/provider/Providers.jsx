a848cc96aedbc0a3dfae414b5c7542b6f3c84cb4
shakir raza <shakirraza737@gmail.com>
2024-10-01 22:07:19 +0300

    initial comit

--- src/provider/Providers.jsx
+++ src/provider/Providers.jsx
@@ -2,7 +2,7 @@
 import { SessionProvider } from "next-auth/react";
 export default function Providers({ children }) {
   return (
-    <SessionProvider basePath="http://localhost:3000/api/auth">
+    <SessionProvider basePath="https://main.dkxdmlhjc83qu.amplifyapp.com/api/auth">
       {children}
     </SessionProvider>
   );
