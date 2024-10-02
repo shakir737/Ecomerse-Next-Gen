047203e49a358bd2650e8229008c830b2e7bc595
shakir raza <shakirraza737@gmail.com>
2024-10-02 14:22:02 +0300

    product main page

---
+++ src/app/(ecommerce)/page.js
@@ -0,0 +1,48 @@
+import CategoryList from "@/components/CategoryList";
+import { Separator } from "@/components/ui/separator";
+import prisma from "@/utils/connection";
+
+export default async function Home() {
+  const dynamic = (await import("next/dynamic")).default;
+  const Fashsales = dynamic(() => import("@/components/Fashsales"));
+  const Carousels = dynamic(() => import("@/components/Carousels"));
+  const Featured = dynamic(() => import("@/components/Featured"));
+  const Enhancement = dynamic(() => import("@/components/Enhancement"));
+  const query = {
+    take: 6,
+    skip: 0,
+  };
+  const categories = await prisma?.category.findMany(query);
+  const popularProducts = await prisma?.product.findMany({
+    ...query,
+    orderBy: { views: "desc" },
+  });
+  const products = await prisma?.product.findMany(query);
+
+  return (
+    <div className="">
+      <Carousels />
+      <div className="px-[10%]">
+        {products && (
+          <Fashsales
+            title="Today's"
+            heading="Flash sales"
+            products={products}
+          />
+        )}
+
+        <Separator className="my-4" />
+        {categories && <CategoryList categories={categories} />}
+
+        <Enhancement />
+        <Separator className="my-4" />
+        <Fashsales
+          title="Our Products"
+          heading="Explore Our Products"
+          products={popularProducts}
+        />
+        <Featured />
+      </div>
+    </div>
+  );
+}
